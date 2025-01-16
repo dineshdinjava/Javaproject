887b7c135b53fcce83bc9b26ec28bed1534b3ff1
Dinesh C <cdinesh11223@gmail.com>
2025-01-16 11:57:05 +0530

    Add files via upload

---
+++ Print10to1dowhile/src/print10to1dowhile.java
@@ -0,0 +1,11 @@
+		
+		public class print10to1dowhile {
+		    public static void main(String[] args) {
+		        int number = 10; // Starting number
+
+		        do {
+		            System.out.println(number); // Print the current number
+		            number--; // Decrement the number
+		        } while (number >= 1); // Loop until the number is greater than or equal to 1
+		    }
+		}
